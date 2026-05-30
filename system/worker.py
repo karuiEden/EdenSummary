8eaa67c7eac75d70a95f2d0cbbf3c1a67a2e4bf6
karuiEden <97159954+karuiEden@users.noreply.github.com>
2026-05-30 12:24:46 +0300

    celery config

--- system/worker.py
+++ system/worker.py
@@ -5,7 +5,7 @@ from system.config import get_celery_cfg
 
 cfg = get_celery_cfg()
 redis_url = f'redis://:{cfg.redis_password}@redis:6380/0'
-celery_app = Celery('worker', broker=cfg.redis_url, backend=cfg.redis_url)
+celery_app = Celery('worker', broker=redis_url, backend=redis_url)
 
 @celery_app.task
 def process_job(job_id: str):
