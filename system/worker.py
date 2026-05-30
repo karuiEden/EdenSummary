bd9d7865b4f7744c98b6c518c7b8a0f7508b7052
karuiEden <97159954+karuiEden@users.noreply.github.com>
2026-05-30 12:25:48 +0300

    celery config

--- system/worker.py
+++ system/worker.py
@@ -4,7 +4,7 @@ from system import pipeline
 from system.config import get_celery_cfg
 
 cfg = get_celery_cfg()
-redis_url = f'redis://:{cfg.redis_password}@redis:6380/0'
+redis_url = f'redis://:{cfg.redis_password}@redis:6379/0'
 celery_app = Celery('worker', broker=redis_url, backend=redis_url)
 
 @celery_app.task
